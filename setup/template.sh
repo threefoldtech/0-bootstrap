18af6edb24ab9b6d03a5bdfce1fe8f2be833df67
Maxime Daniel <root@maxux.net>
2017-06-13 12:16:45 +0200

    Adding pre-compiled uefi source in template

--- setup/template.sh
+++ setup/template.sh
@@ -2,6 +2,7 @@
 set -e
 
 template="/opt/ipxe-template"
+templateuefi="/opt/ipxe-template-uefi"
 
 echo "[+] preparing iPXE template on: ${template}"
 
@@ -9,25 +10,43 @@ echo "[+] downloading source code"
 pushd /tmp
 git clone --depth=1 git://git.ipxe.org/ipxe.git
 
-echo "[+] pre-compiling"
-pushd ipxe/src
+echo "[+] preparing images"
+cp -r ipxe ipxe-legacy
+cp -r ipxe ipxe-uefi
 
-# Enable HTTPS
-sed -i "s/undef\tDOWNLOAD_PROTO_HTTPS/define\tDOWNLOAD_PROTO_HTTPS/" config/general.h
+for target in "ipxe-legacy" "ipxe-uefi"; do
+    echo "[+] preparing: ${target}"
+    pushd ${target}/src
 
-wget https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem.txt -O letsencrypt-x3-cross.crt
-wget https://letsencrypt.org/certs/letsencryptauthorityx3.pem.txt -O letsencrypt-x3.crt
+    # Enable HTTPS
+    sed -i "s/undef\tDOWNLOAD_PROTO_HTTPS/define\tDOWNLOAD_PROTO_HTTPS/" config/general.h
+
+    wget -4 https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem.txt -O letsencrypt-x3-cross.crt
+    wget -4 https://letsencrypt.org/certs/letsencryptauthorityx3.pem.txt -O letsencrypt-x3.crt
+
+    popd
+done
 
 MKCERT="letsencrypt-x3-cross.crt,letsencrypt-x3.crt"
 MKTRUST="letsencrypt-x3-cross.crt,letsencrypt-x3.crt"
 
+echo "[+] pre-compiling: ipxe-legacy"
+pushd ipxe-legacy/src
 make -j 5 bin/ipxe.iso CERT=${MKCERT} TRUST=${MKTRUST}
 make -j 5 bin/ipxe.usb CERT=${MKCERT} TRUST=${MKTRUST}
-
-echo "[+] installing template"
+make -j 5 bin/ipxe.lkrn CERT=${MKCERT} TRUST=${MKTRUST}
 popd
-cp -ar ipxe/src ${template}
+
+echo "[+] pre-compiling: ipxe-uefi"
+pushd ipxe-uefi/src
+make -j 5 bin-x86_64-efi/ipxe.efi CERT=${MKCERT} TRUST=${MKTRUST}
+popd
+
+echo "[+] installing templates"
+cp -ar ipxe-legacy/src ${template}
+cp -ar ipxe-uefi/src ${templateuefi}
 
 echo "[+] ================================================================"
-echo "[+] iPXE template installed on: ${template}"
+echo "[+] iPXE legacy template installed on: ${template}"
+echo "[+] iPXE uefi template installed on: ${templateuefi}"
 echo "[+] ================================================================"
