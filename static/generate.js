32a8086cc451143185fda7707a7aad2f9e041d7b
Maxime Daniel <root@maxux.net>
2018-07-23 09:26:09 +0200

    Improve generator by processing fields and jwt
    Improve the kernel argument generator by adding Organization and Farmer
    ID field explicitly and validating the jwt provided on the Farmer ID
    before allowing to download the kernel

--- static/generate.js
+++ static/generate.js
@@ -1,6 +1,7 @@
 var allValid = false;
 var allValidReason = '';
 
+var jwtkargs = null;
 var buildlist = ['ipxe', 'iso', 'usb', 'uefi', 'krn'];
 var finalUrl = '...';
 var items = {
@@ -8,6 +9,46 @@ var items = {
     'zerotier': '',
     'kargs': '',
 };
+
+function jwtvalidate(jwt) {
+    // itsyouonline public key
+    var key = "-----BEGIN PUBLIC KEY-----\n" +
+              "MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAES5X8XrfKdx9gYayFITc89wad4usrk0n2\n" +
+              "7MjiGYvqalizeSWTHEpnd7oea9IQ8T5oJjMVH5cc0H5tFSKilFFeh//wngxIyny6\n" +
+              "6+Vq5t5B0V0Ehy01+2ceEon2Y0XDkIKv\n" +
+              "-----END PUBLIC KEY-----";
+
+    var acceptField = {
+        'alg': ['ES384'],
+        'verifyAt': KJUR.jws.IntDate.get("0"),
+        'iss': ['itsyouonline'],
+    };
+
+    var isValid = true;
+    var organization = null;
+
+    try {
+        isValid = KJUR.jws.JWS.verifyJWT(jwt, key, acceptField);
+
+        if(isValid) {
+            var a = jwt.split(".");
+            var uClaim = b64utos(a[1]);
+            var pClaim = KJUR.jws.JWS.readSafeJSONString(uClaim);
+
+            if(pClaim['scope'].length != 1)
+                return false;
+
+            organization = pClaim['scope'][0];
+            return organization.substr(14, 1000);
+        }
+
+    } catch(ex) {
+        console.log(ex);
+        isValid = false;
+    }
+
+    return isValid;
+}
 
 function setValid(id, value, reason) {
     if(value) {
@@ -22,8 +63,42 @@ function setValid(id, value, reason) {
     }
 }
 
-function update_trigger() {
-    allValid = true;
+function farmerid() {
+    var token = document.getElementById('kargs-farmer').value;
+
+    if((organization = jwtvalidate(token)) !== false) {
+        document.getElementById('kargs-farmer-org').innerHTML = '<strong>' + organization + '</strong>';
+        return true;
+    }
+
+    setValid('kargs-farmer', false, 'Invalid Farmer ID token');
+    return false;
+}
+
+function update_trigger_jwt() {
+    // reset any flags
+    jwtkargs = null;
+    setValid('kargs-farmer', true);
+    document.getElementById('kargs-farmer-org').innerHTML = '';
+
+    // if jwt is not set, nothing to do
+    if(!document.getElementById('kargs-farmer').value)
+        return update_trigger(true);
+
+    // checking jwt validity
+    if(farmerid()) {
+        jwtkargs = 'farmer_id=' + document.getElementById('kargs-farmer').value;
+        return update_trigger(true);
+    }
+
+    // call default trigger
+    return update_trigger(false);
+}
+
+function update_trigger(initialAllValid) {
+    allValid = (initialAllValid == undefined) ? true : initialAllValid;
+
+    var kargs = [];
 
     items['branch'] = document.getElementById('branch').value;
     setValid('branch', (items['branch'] != '----'), "The selected branch is not valid");
@@ -35,8 +110,25 @@ function update_trigger() {
 
     setValid('ztnet', (items['zerotier'] == '0' || items['zerotier'].length == 16), "Invalid Zerotier Netork ID");
 
-    items['kargs'] = document.getElementById('kargs').value;
-    setValid('kargs', true);
+    // auto validate organization
+    setValid('kargs-org', true);
+
+    if(document.getElementById('kargs-org').value) {
+        kargs.push('organization="' + document.getElementById('kargs-org').value + '"');
+    }
+
+    if(document.getElementById('development').checked)
+        kargs.push("development");
+
+    if(document.getElementById('debug').checked)
+        kargs.push("debug");
+
+    // append global jwt (if set)
+    if(jwtkargs)
+        kargs.push(jwtkargs);
+
+    // populates extra arguments
+    items['kargs'] = kargs.join(" ");
 
     update_url();
 }
