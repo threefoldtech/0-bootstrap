9b1412858ff24947cf52f2c7197f2f273925dfc0
Maxime Daniel <root@maxux.net>
2020-03-20 15:07:51 +0100

    Update farmer id input validation (fixes #4)

--- static/generate.js
+++ static/generate.js
@@ -8,25 +8,53 @@ var finalUrl = '...';
 var tfmodes = {"prod": "success", "test": "warning", "dev": "danger"};
 var tfmode = "prod";
 
-function update_trigger(initialAllValid) {
-    if($("#farmerid").val() == "") {
-        allValid = false;
-        $("#farmerid").removeClass("is-valid");
-        $("#farmerid").addClass("is-invalid");
-        allValidReason = 'Please set your Farmer ID';
-
-    } else {
-        allValid = true;
-        $("#farmerid").removeClass("is-invalid");
-        $("#farmerid").addClass("is-valid");
-    }
+function farmerid_invalid() {
+    allValid = false;
+    $("#farmerid").removeClass("is-valid");
+    $("#farmerid").addClass("is-invalid");
+    allValidReason = 'Please fix your Farmer ID';
 
     update_url();
 }
 
+function farmerid_valid() {
+    allValid = true;
+    $("#farmerid").removeClass("is-invalid");
+    $("#farmerid").addClass("is-valid");
+
+    update_url();
+}
+
+function update_trigger(initialAllValid) {
+    var fid = $("#farmerid").val();
+
+    // farmer id needs to be a positive non null integer with no coma or dot
+    // check related to issue #4
+    fid = fid.split('.').join("").split(',').join("");
+
+    if(/\D/.test(fid)) {
+        $('#farmerid-cleared').html("Invalid");
+        return farmerid_invalid();
+    }
+
+    if(fid == "") {
+        $('#farmerid-cleared').html("Missing");
+        return farmerid_invalid();
+    }
+
+    fid = parseInt(fid);
+
+    $('#farmerid-cleared').html(fid);
+
+    if(Number.isInteger(fid) && fid > 0)
+        return farmerid_valid();
+
+    return farmerid_invalid();
+}
+
 function update_url() {
     if(!allValid) {
-        document.getElementById('userurl').innerHTML = 'Your URL is not ready yet. ' + allValidReason;
+        // document.getElementById('userurl').innerHTML = 'Your URL is not ready yet. ' + allValidReason;
         update_urls_fails();
 
         $("#jumbofinal").addClass('jumbo-nok');
@@ -38,7 +66,7 @@ function update_url() {
     $("#jumbofinal").removeClass('jumbo-nok');
     $("#jumbofinal").addClass('jumbo-ok');
 
-    var userurl = '/' + tfmode + '/' + $("#farmerid").val();
+    var userurl = '/' + tfmode + '/' + $("#farmerid-cleared").html();
 
     finalUrl = userurl;
     $('#userurl').html(userurl);
