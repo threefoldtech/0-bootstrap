319d6b532bd680d07a282bc830071f2bb4de3aef
Maxime Daniel <root@maxux.net>
2021-01-06 01:50:03 +0100

    setup: update letsencrypt https setup

--- setup/template.sh
+++ setup/template.sh
@@ -6,17 +6,25 @@ templateuefi="/opt/ipxe-template-uefi"
 makeopts="-j 10"
 
 echo "[+] preparing ipxe template on: ${template}"
+pushd /tmp
+
+echo "[+] clean up previous stuff"
+rm -rf ipxe
+rm -rf ipxe-legacy
+rm -rf ipxe-legacy
 
 echo "[+] downloading source code"
-pushd /tmp
 git clone --depth=1 https://github.com/gigforks/ipxe
+
+wget https://letsencrypt.org/certs/lets-encrypt-r3-cross-signed.pem -O ipxe/lets-encrypt-r3-cross-signed.pem
+wget https://letsencrypt.org/certs/lets-encrypt-r3.pem -O ipxe/lets-encrypt-r3.pem
+wget https://letsencrypt.org/certs/isrgrootx1.pem -O ipxe/isrgrootx1.pem
+
+mkcert="isrgrootx1.pem,lets-encrypt-r3-cross-signed.pem,lets-encrypt-r3.pem"
 
 echo "[+] preparing images"
 cp -r ipxe ipxe-legacy
 cp -r ipxe ipxe-uefi
-
-mkcert="letsencrypt-x3-cross.crt,letsencrypt-x3.crt"
-mktrust="letsencrypt-x3-cross.crt,letsencrypt-x3.crt"
 
 echo "[+] pre-compiling: ipxe-legacy"
 pushd ipxe-legacy/src
@@ -29,6 +37,10 @@ echo "[+] pre-compiling: ipxe-uefi"
 pushd ipxe-uefi/src
 make ${makeopts} bin-x86_64-efi/ipxe.efi CERT=${mkcert} TRUST=${mktrust}
 popd
+
+echo "[+] removing previous installation"
+rm -rf ${template}
+rm -rf ${templateuefi}
 
 echo "[+] installing templates"
 cp -ar ipxe-legacy/src ${template}
