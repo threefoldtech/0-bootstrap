1e8f30bb2127903d0573ff88571f6354415b4ab6
Maxime Daniel <root@maxux.net>
2022-01-24 15:35:28 +0100

    uefi: add snponly build script

---
+++ scripts/mksnponly.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ "$1" == "" ]; then
+    echo "[-] missing root path"
+    exit 1
+fi
+
+root="$1"
+echo "[+] root is: ${root}"
+
+cat ${root}/boot.ipxe
+
+pushd ${root}/src
+
+MKCERT="isrgrootx1.pem,lets-encrypt-r3.pem,lets-encrypt-r3-cross-signed.pem"
+MKTRUST=${MKCERT}
+
+make bin-x86_64-efi/snponly.efi EMBED=${root}/boot.ipxe CERT=${MKCERT} TRUST=${MKTRUST}
+
+
+cp bin-x86_64-efi/snponly.efi ${root}/ipxe.efi
